4c32f6f64c8f0b522a7fe2307cde8f12fecb3766
march-dave <ysangy@gmail.com>
2016-04-27 19:53:43 -0700

    Filter add

--- public/main.js
+++ public/main.js
@@ -1,1 +1,50 @@
 'user strict';
+
+
+var app = angular.module('myApp', []);
+
+app.controller('mainCtrl', function($scope) {
+
+$scope.balances =
+  [{
+    date: "2000-01-12",
+    desc: "lunch",
+    debits:  "$20",
+    credits: ""
+  },
+  {
+    date: "2011-11-12",
+    desc: "payday",
+    debits: "",
+    credits: "$500"
+  }];
+
+$scope.addBalance = function() {
+  $scope.balances.push($scope.newBalance);
+  $scope.newBalance = {};
+};
+
+$scope.removeBalance = function(balance) {
+  var index = $scope.balances.indexOf(balance);
+
+  $scope.balances.splice(index, 1);
+};
+
+$scope.sortBy  = function(order) {
+  if($scope.sortOrder === order) {
+    $scope.sortOrder = `-${order}`;
+  } else if ($scope.sortOrder === `-${order}`) {
+    $scope.sortOrder = '';
+  } else {
+    $scope.sortOrder = order;
+  }
+};
+
+
+
+
+
+
+
+
+});
